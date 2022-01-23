11eb55c4f74bb6c721247b772742f2bc1fd5e014
Denys <denis200358@gmail.com>
2022-01-23 02:50:57 +0200

    added public rooms list

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,24 +1,48 @@
-import React from "react";
+import React, {useEffect, useRef, useState } from "react";
 import {Footer} from "../../common";
 import {Navigation} from "../../components";
-import {LinkIcon, VideoCameraIcon} from '@iconicicons/react';
+import {VideoCameraIcon} from '@iconicicons/react';
 import "./style.scss";
+import {useNavigate} from "react-router-dom";
+import {v4} from 'uuid';
+import socket from "../../socket";
+import { ACTIONS } from "../../socket/actions";
+
 export default (props: any) => {
+    const history = useNavigate();
+    const [rooms, updateRooms] = useState([]);
+    const rootNode = useRef();
+
+    useEffect(() => {
+        socket.on(ACTIONS.SHARE_ROOMS, ({rooms = []} = {}) => {
+            if (rootNode.current) {
+                updateRooms(rooms);
+            }
+        });
+    }, []);
     return (
         <div className={"container"}>
             <Navigation/>
             <div className={"wrap wrap-part-width full-height dashboard-content"}>
                 <h2>Welcome home #USER</h2>
                 <div className={"action-user"}>
-                    <button className={"action"}>
+                    <button onClick={()=> history(`/room/${v4()}`)} className={"action"}>
                         <VideoCameraIcon/>
                         <span>Create a meeting</span>
                     </button>
-                    <button className={"action"}>
-                        <LinkIcon/>
-                        <span>Connect to existing</span>
-                    </button>
                 </div>
+                <h1>Available Rooms</h1>
+
+                <ul>
+                    {rooms.map(roomID => (
+                        <li key={roomID}>
+                            {roomID}
+                            <button onClick={() => {
+                                history(`/room/${roomID}`);
+                            }}>JOIN ROOM</button>
+                        </li>
+                    ))}
+                </ul>
             </div>
             <Footer/>
         </div>
